fd3c6c92bc9851b8e60153ab761ed1ca42057ed5
Sam Prakash Royyala <samprakash1410@gmail.com>
2024-11-21 11:31:15 +0530

    added changes

---
+++ main.tf
@@ -0,0 +1,33 @@
+data "aws_vpc" "default" {
+  default = true
+}
+module "web_sg" {
+  source = "./module/my_security_group"
+  default = {
+    name        = "web"
+    description = "this is web security group"
+    vpc_id      = data.aws_vpc.default.id
+    rules = [{
+      from_port   = "22"
+      to_port     = "22"
+      type        = "ingress"
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+      },
+      {
+        from_port   = "80"
+        to_port     = "80"
+        type        = "ingress"
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      },
+      {
+        from_port   = "443"
+        to_port     = "443"
+        type        = "ingress"
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+    ]
+  }
+}
